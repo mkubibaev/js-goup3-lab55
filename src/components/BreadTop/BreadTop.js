9015f6d167964622b0382545cf198a4296f61aa4
Alina Bibikova <alinochkakleopatra@gmail.com>
2018-12-25 19:37:49 +0600

    added BreadTop component

---
+++ src/components/BreadTop/BreadTop.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const BreadTop = props => (
+    <div className='BreadTop'>
+        <div className="Seeds1"></div>
+        <div className="Seeds2"></div>
+    </div>
+);
+
+export default BreadTop;
